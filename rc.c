92780cd4ab55530f49ae44b02b874140f16279f3
Fumitoshi UKAI <ukai@debian.or.jp>
2001-11-30 19:59:26 +0000

    run make indent

--- rc.c
+++ rc.c
@@ -1,4 +1,4 @@
-/* $Id: rc.c,v 1.21 2001/11/30 14:06:27 ukai Exp $ */
+/* $Id: rc.c,v 1.22 2001/11/30 19:59:26 ukai Exp $ */
 /* 
  * Initialization file etc.
  */
@@ -1095,7 +1095,7 @@ sync_with_option(void)
     if (AcceptLang == NULL || *AcceptLang == '\0') {
 #if LANG == JA
 	AcceptLang = "ja; q=1.0, en; q=0.5";
-#else	/* LANG != JA (must be EN) */
+#else				/* LANG != JA (must be EN) */
 	AcceptLang = "en; q=1.0";
 #endif
     }

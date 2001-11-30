47dc10085ac70868675a0f8b017e67f266f8a180
Fumitoshi UKAI <ukai@debian.or.jp>
2001-11-30 04:52:57 +0000

    [w3m-dev 02589] From: Hironori Sakamoto <hsaka@mth.biglobe.ne.jp>

--- rc.c
+++ rc.c
@@ -1,4 +1,4 @@
-/* $Id: rc.c,v 1.16 2001/11/29 09:34:15 ukai Exp $ */
+/* $Id: rc.c,v 1.17 2001/11/30 04:52:57 ukai Exp $ */
 /* 
  * Initialization file etc.
  */
@@ -386,10 +386,8 @@ struct param_ptr params1[] = {
      NULL},
     {"ignore_null_img_alt", P_INT, PI_ONOFF, (void *)&ignore_null_img_alt,
      CMT_IGNORE_NULL_IMG_ALT, NULL},
-#ifdef VIEW_UNSEENOBJECTS
     {"view_unseenobject", P_INT, PI_ONOFF, (void *)&view_unseenobject,
      CMT_VIEW_UNSEENOBJECTS, NULL},
-#endif				/* VIEW_UNSEENOBJECTS */
     {"show_lnum", P_INT, PI_ONOFF, (void *)&showLineNum, CMT_SHOW_NUM, NULL},
     {NULL, 0, 0, NULL, NULL, NULL},
 };

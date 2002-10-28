1cf8872ba15eeee7cddbbb74e3b93c647d597a5b
Fumitoshi UKAI <ukai@debian.or.jp>
2002-10-28 04:53:07 +0000

    [w3m-dev 03351] w3m core dump * table.c (feed_table_tag): fix rendering problem (maybe security hole?) From: Hironori SAKAMOTO <hsaka@mth.biglobe.ne.jp>

--- table.c
+++ table.c
@@ -1,4 +1,4 @@
-/* $Id: table.c,v 1.25 2002/10/10 16:59:33 ukai Exp $ */
+/* $Id: table.c,v 1.26 2002/10/28 04:53:07 ukai Exp $ */
 /* 
  * HTML table
  */
@@ -2679,10 +2679,11 @@ feed_table_tag(struct table *tbl, char *line, struct table_mode *mode,
 #if 0
 		tbl->tabattr[tbl->row + i][tbl->col + j] &= ~(HTT_X | HTT_Y);
 #endif
-		if (tbl->tabattr[tbl->row + i][tbl->col + j] & (HTT_X | HTT_Y))
-		    break;
-		tbl->tabattr[tbl->row + i][tbl->col + j] |=
-		    ((i > 0) ? HTT_Y : 0) | ((j > 0) ? HTT_X : 0);
+		if (! (tbl->tabattr[tbl->row + i][tbl->col + j] &
+			(HTT_X | HTT_Y))) {
+		    tbl->tabattr[tbl->row + i][tbl->col + j] |=
+			((i > 0) ? HTT_Y : 0) | ((j > 0) ? HTT_X : 0);
+		}
 		if (tbl->col + j > tbl->maxcol) {
 		    tbl->maxcol = tbl->col + j;
 		}

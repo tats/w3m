fdfba3d8518f571646960381aac8bb859ab486d6
Fumitoshi UKAI <ukai@debian.or.jp>
2002-12-05 16:49:45 +0000

    fix indent

--- table.c
+++ table.c
@@ -1,4 +1,4 @@
-/* $Id: table.c,v 1.27.4.2 2002/11/26 07:10:18 ukai Exp $ */
+/* $Id: table.c,v 1.27.4.3 2002/12/05 16:49:45 ukai Exp $ */
 /* 
  * HTML table
  */
@@ -1544,7 +1544,7 @@ check_table_height(struct table *t)
 		    cell.rowspan[cell.maxcell] = rowspan;
 		    cell.height[cell.maxcell] = 0;
 		    if (cell.maxcell > k) {
-	    		int ii;
+			int ii;
 			for (ii = k; ii < cell.maxcell; ii++)
 			    cell.indexarray[ii + 1] = cell.indexarray[ii];
 		    }
@@ -2661,7 +2661,7 @@ feed_table_tag(struct table *tbl, char *line, struct table_mode *mode,
 		cell->minimum_width[cell->maxcell] = 0;
 		cell->fixed_width[cell->maxcell] = 0;
 		if (cell->maxcell > k) {
-	    	    int ii;
+		    int ii;
 		    for (ii = k; ii < cell->maxcell; ii++)
 			cell->index[ii + 1] = cell->index[ii];
 		}

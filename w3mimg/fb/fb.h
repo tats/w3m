416d70d11e3cece366b68c2bd8fa81bb552155b5
Yusuke Baba <babayaga1@y8.dion.ne.jp>
2014-12-03 22:29:39 +0900

    Support FreeBSD framebuffer
    Origin: http://www.ac.auone-net.jp/~baba/w3m-img/index.html
    Bug-FreeBSD: https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=122673

--- w3mimg/fb/fb.h
+++ w3mimg/fb/fb.h
@@ -1,7 +1,11 @@
 /* $Id: fb.h,v 1.7 2003/07/07 15:48:17 ukai Exp $ */
 #ifndef fb_header
 #define fb_header
+#if defined(__linux__)
 #include <linux/fb.h>
+#elif defined(__FreeBSD__)
+#include <sys/fbio.h> 
+#endif
 
 typedef struct {
     int num;

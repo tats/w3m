928af643ca0ecfb25467e35df6a9d4de814d8173
Tatsuya Kinoshita <tats@debian.org>
2015-05-03 11:44:14 +0900

    Correct GC version confirmation

--- main.c
+++ main.c
@@ -908,7 +908,7 @@ main(int argc, char **argv, char **envp)
     mySignal(SIGPIPE, SigPipe);
 #endif
 
-#if GC_VERSION_MAJOR >= 7 && GC_VERSION_MINOR >= 2
+#if (GC_VERSION_MAJOR>7) || ((GC_VERSION_MAJOR==7) && (GC_VERSION_MINOR>=2))
     orig_GC_warn_proc = GC_get_warn_proc();
     GC_set_warn_proc(wrap_GC_warn_proc);
 #else

dd9d2a9752b03453dccf09a0f6c9c4bb7f727830
David Crosby <dave@dafyddcrosby.com>
2015-07-21 23:05:54 -0600

    Remove unused value

--- file.c
+++ file.c
@@ -7208,7 +7208,6 @@ loadHTMLstream(URLFile *f, Buffer *newBuf, FILE * src, int internal)
 	HTMLlineproc0(lineBuf2->ptr, &htmlenv1, internal);
     }
     if (obuf.status != R_ST_NORMAL) {
-	obuf.status = R_ST_EOL;
 	HTMLlineproc0("\n", &htmlenv1, internal);
     }
     obuf.status = R_ST_NORMAL;

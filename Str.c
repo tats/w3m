e11bb2b868940d9390510b8b7bbd482f74f4baa7
Tatsuya Kinoshita <tats@debian.org>
2021-03-04 22:18:03 +0900

    Consider Strgrow overflow in Strinsert_char

--- Str.c
+++ Str.c
@@ -348,10 +348,12 @@ Strinsert_char(Str s, int pos, char c)
 {
     int i;
     STR_LENGTH_CHECK(s);
-    if (pos < 0 || s->length < pos)
+    if (pos < 0)
 	return;
     if (s->length + 2 > s->area_size)
 	Strgrow(s);
+    if (s->length < pos)
+	return;
     for (i = s->length; i > pos; i--)
 	s->ptr[i] = s->ptr[i - 1];
     s->ptr[++s->length] = '\0';

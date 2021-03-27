d0a30912c83f90595ded23fd3924ba826f498b16
Tatsuya Kinoshita <tats@debian.org>
2021-03-27 20:04:56 +0900

    Fix potential integer overflow in allocStr

--- indep.c
+++ indep.c
@@ -104,6 +104,8 @@ allocStr(const char *s, int len)
 	return NULL;
     if (len < 0)
 	len = strlen(s);
+    if (len < 0 || len >= STR_SIZE_MAX)
+	len = STR_SIZE_MAX - 1;
     ptr = NewAtom_N(char, len + 1);
     if (ptr == NULL) {
 	fprintf(stderr, "fm: Can't allocate string. Give me more memory!\n");

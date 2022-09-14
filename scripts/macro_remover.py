bd2d046e3e0fc45babd7ca538272a95d56177e16
ousttrue <ousttrue@gmail.com>
2022-09-15 01:42:03 +0900

    macro_remover

--- scripts/macro_remover.py
+++ scripts/macro_remover.py
@@ -1,8 +1,14 @@
 import sys
 import pathlib
 import re
-from typing import NamedTuple, Union
+from typing import NamedTuple, Union, List, Optional
 from enum import Enum, auto
+from termcolor import colored
+
+CONTEXT = {
+    'USE_M17N': True,
+    'USE_UNICODE': True,
+}
 
 MACRO_PATTERN = re.compile(r'^#\s*(\S+)\s*(\S.*)?')
 
@@ -18,21 +24,36 @@ class Define(NamedTuple):
 class If(NamedTuple):
     value: str
 
+    def eval(self, context) -> Optional[bool]:
+        return None
+
 
 class Ifdef(NamedTuple):
     value: str
+
+    def eval(self, context) -> Optional[bool]:
+        if self.value in context:
+            return True
 
 
 class Ifndef(NamedTuple):
     value: str
 
+    def eval(self, context) -> Optional[bool]:
+        return None
+
 
 class Elif(NamedTuple):
     value: str
 
+    def eval(self, context) -> Optional[bool]:
+        return None
+
 
 class Else(NamedTuple):
-    pass
+
+    def eval(self, context) -> bool:
+        return True
 
 
 class Endif(NamedTuple):
@@ -61,8 +82,62 @@ def parse_macro(l: str) -> Union[None, Include, Define, If, Ifdef, Ifndef, Else,
                 return Endif()
 
 
+class MacroNode:
+    def __init__(self, begin: int, begin_macro, prev=None) -> None:
+        self.prev = prev
+        self.begin = begin
+        self.begin_macro = begin_macro
+        self.end = begin
+        self.end_macro = None
+        self.children: List[MacroNode] = []
+        self.result = None
+
+    def close(self, end: int, end_macro):
+        self.end = end
+        self.end_macro = end_macro
+
+    def eval(self, context):
+        match self.begin_macro:
+            case None:
+                pass
+            case If() | Ifndef() | Ifdef() | Elif() | Else() as m:
+                has_true = False
+                current = self
+                while True:
+                    if not current.prev:
+                        break
+                    current = current.prev
+                    if current.result:
+                        has_true = True
+                        break
+                if not has_true:
+                    self.result = m.eval(context)
+            case _:
+                raise NotImplementedError()
+        for child in self.children:
+            child.eval(context)
+
+    def print(self, indent=''):
+        match self.result:
+            case True:
+                color = 'green'
+            case False:
+                color = 'red'
+            case None:
+                color = 'grey'
+            case _:
+                raise NotImplementedError()
+
+        print(colored(f'{self.begin:04} ~ {self.end:04}:{indent}{self.begin_macro}', color))
+        for child in self.children:
+            child.print(indent + '  ')
+
+
 def main(path: pathlib.Path):
-    for l in path.read_text().splitlines():
+    lines = path.read_text().splitlines()
+    root = MacroNode(0, None)
+    stack = [root]
+    for i, l in enumerate(lines):
         if l.startswith('#'):
             match parse_macro(l):
                 case Include() as m:
@@ -71,22 +146,28 @@ def main(path: pathlib.Path):
                 case Define() as m:
                     pass
                     # print(f'[DEFINE] => {m}')
-                case If() as m:
-                    pass
-                    # print(f'[If] => {m}')
-                case Ifndef() as m:
-                    pass
-                case Ifdef() as m:
-                    pass
-                case Elif() as m:
-                    pass
-                case Else() as m:
-                    pass
+                case If() | Ifndef() | Ifdef() as m:
+                    node = MacroNode(i, m)
+                    stack[-1].children.append(node)
+                    stack.append(node)
+                case Elif() | Else() as m:
+                    stack[-1].close(i, m)
+                    stack.pop()
+                    # new node
+                    node = MacroNode(i, m)
+                    stack[-1].children.append(node)
+                    stack.append(node)
                 case Endif() as m:
+                    stack[-1].close(i, m)
+                    stack.pop()
                     pass
                 case _:
                     print(l)
                     # print(l)
+    assert (len(stack) == 1)
+    root.close(len(lines) - 1, None)
+    root.eval(CONTEXT)
+    root.print()
 
 
 if __name__ == '__main__':
